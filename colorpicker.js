3ff5ccde314feff2128f80e39c9319bef1c7b706
Delapouite <delapouite@gmail.com>
2013-09-26 13:16:11 +0200

    misc hinting

--- colorpicker.js
+++ colorpicker.js
@@ -9,9 +9,8 @@
 
     // This HTML snippet is inserted into the innerHTML property of the passed color picker element
     // when the no-hassle call to ColorPicker() is used, i.e. ColorPicker(function(hex, hsv, rgb) { ... });
-    
+
     var colorpickerHTMLSnippet = [
-        
         '<div class="picker-wrapper">',
                 '<div class="picker"></div>',
                 '<div class="picker-indicator"></div>',
@@ -20,7 +19,6 @@
                 '<div class="slide"></div>',
                 '<div class="slide-indicator"></div>',
         '</div>'
-        
     ].join('');
 
     /**
@@ -58,50 +56,49 @@
      * Create slide and picker markup depending on the supported technology.
      */
     if (type == 'SVG') {
-
         slide = $('svg', { xmlns: 'http://www.w3.org/2000/svg', version: '1.1', width: '100%', height: '100%' },
-                  [
-                      $('defs', {},
-                        $('linearGradient', { id: 'gradient-hsv', x1: '0%', y1: '100%', x2: '0%', y2: '0%'},
-                          [
-                              $('stop', { offset: '0%', 'stop-color': '#FF0000', 'stop-opacity': '1' }),
-                              $('stop', { offset: '13%', 'stop-color': '#FF00FF', 'stop-opacity': '1' }),
-                              $('stop', { offset: '25%', 'stop-color': '#8000FF', 'stop-opacity': '1' }),
-                              $('stop', { offset: '38%', 'stop-color': '#0040FF', 'stop-opacity': '1' }),
-                              $('stop', { offset: '50%', 'stop-color': '#00FFFF', 'stop-opacity': '1' }),
-                              $('stop', { offset: '63%', 'stop-color': '#00FF40', 'stop-opacity': '1' }),
-                              $('stop', { offset: '75%', 'stop-color': '#0BED00', 'stop-opacity': '1' }),
-                              $('stop', { offset: '88%', 'stop-color': '#FFFF00', 'stop-opacity': '1' }),
-                              $('stop', { offset: '100%', 'stop-color': '#FF0000', 'stop-opacity': '1' })
-                          ]
-                         )
-                       ),
-                      $('rect', { x: '0', y: '0', width: '100%', height: '100%', fill: 'url(#gradient-hsv)'})
-                  ]
-                 );
+                    [
+                        $('defs', {},
+                            $('linearGradient', { id: 'gradient-hsv', x1: '0%', y1: '100%', x2: '0%', y2: '0%'},
+                                [
+                                    $('stop', { offset: '0%', 'stop-color': '#FF0000', 'stop-opacity': '1' }),
+                                    $('stop', { offset: '13%', 'stop-color': '#FF00FF', 'stop-opacity': '1' }),
+                                    $('stop', { offset: '25%', 'stop-color': '#8000FF', 'stop-opacity': '1' }),
+                                    $('stop', { offset: '38%', 'stop-color': '#0040FF', 'stop-opacity': '1' }),
+                                    $('stop', { offset: '50%', 'stop-color': '#00FFFF', 'stop-opacity': '1' }),
+                                    $('stop', { offset: '63%', 'stop-color': '#00FF40', 'stop-opacity': '1' }),
+                                    $('stop', { offset: '75%', 'stop-color': '#0BED00', 'stop-opacity': '1' }),
+                                    $('stop', { offset: '88%', 'stop-color': '#FFFF00', 'stop-opacity': '1' }),
+                                    $('stop', { offset: '100%', 'stop-color': '#FF0000', 'stop-opacity': '1' })
+                                ]
+                            )
+                        ),
+                        $('rect', { x: '0', y: '0', width: '100%', height: '100%', fill: 'url(#gradient-hsv)'})
+                    ]
+                );
 
         picker = $('svg', { xmlns: 'http://www.w3.org/2000/svg', version: '1.1', width: '100%', height: '100%' },
-                   [
-                       $('defs', {},
-                         [
-                             $('linearGradient', { id: 'gradient-black', x1: '0%', y1: '100%', x2: '0%', y2: '0%'},
-                               [
-                                   $('stop', { offset: '0%', 'stop-color': '#000000', 'stop-opacity': '1' }),
-                                   $('stop', { offset: '100%', 'stop-color': '#CC9A81', 'stop-opacity': '0' })
-                               ]
-                              ),
-                             $('linearGradient', { id: 'gradient-white', x1: '0%', y1: '100%', x2: '100%', y2: '100%'},
-                               [
-                                   $('stop', { offset: '0%', 'stop-color': '#FFFFFF', 'stop-opacity': '1' }),
-                                   $('stop', { offset: '100%', 'stop-color': '#CC9A81', 'stop-opacity': '0' })
-                               ]
-                              )
-                         ]
+                    [
+                        $('defs', {},
+                            [
+                                $('linearGradient', { id: 'gradient-black', x1: '0%', y1: '100%', x2: '0%', y2: '0%'},
+                                    [
+                                        $('stop', { offset: '0%', 'stop-color': '#000000', 'stop-opacity': '1' }),
+                                        $('stop', { offset: '100%', 'stop-color': '#CC9A81', 'stop-opacity': '0' })
+                                    ]
+                                ),
+                                $('linearGradient', { id: 'gradient-white', x1: '0%', y1: '100%', x2: '100%', y2: '100%'},
+                                    [
+                                        $('stop', { offset: '0%', 'stop-color': '#FFFFFF', 'stop-opacity': '1' }),
+                                        $('stop', { offset: '100%', 'stop-color': '#CC9A81', 'stop-opacity': '0' })
+                                    ]
+                                )
+                            ]
                         ),
-                       $('rect', { x: '0', y: '0', width: '100%', height: '100%', fill: 'url(#gradient-white)'}),
-                       $('rect', { x: '0', y: '0', width: '100%', height: '100%', fill: 'url(#gradient-black)'})
-                   ]
-                  );
+                        $('rect', { x: '0', y: '0', width: '100%', height: '100%', fill: 'url(#gradient-white)'}),
+                        $('rect', { x: '0', y: '0', width: '100%', height: '100%', fill: 'url(#gradient-black)'})
+                    ]
+                );
 
     } else if (type == 'VML') {
         slide = [
@@ -122,7 +119,7 @@
             '</v:rect>',
             '</DIV>'
         ].join('');
-        
+
         if (!document.namespaces['v'])
             document.namespaces.add('v', 'urn:schemas-microsoft-com:vml', '#default#VML');
     }
@@ -134,7 +131,7 @@
     function hsv2rgb(hsv) {
         var R, G, B, X, C;
         var h = (hsv.h % 360) / 60;
-        
+
         C = hsv.v * hsv.s;
         X = C * (1 - Math.abs(h % 2 - 1));
         R = G = B = hsv.v - C;
@@ -156,11 +153,10 @@
      * Credits to http://www.raphaeljs.com
      */
     function rgb2hsv(rgb) {
-
         var r = rgb.r;
         var g = rgb.g;
         var b = rgb.b;
-        
+
         if (rgb.r > 1 || rgb.g > 1 || rgb.b > 1) {
             r /= 255;
             g /= 255;
@@ -182,7 +178,7 @@
     /**
      * Return click event handler for the slider.
      * Sets picker background color and calls ctx.callback if provided.
-     */  
+     */
     function slideListener(ctx, slideElement, pickerElement) {
         return function(evt) {
             evt = evt || window.event;
@@ -192,29 +188,29 @@
             var c = hsv2rgb({ h: ctx.h, s: 1, v: 1 });
             pickerElement.style.backgroundColor = c.hex;
             ctx.callback && ctx.callback(c.hex, { h: ctx.h - hueOffset, s: ctx.s, v: ctx.v }, { r: c.r, g: c.g, b: c.b }, undefined, mouse);
-        }
-    };
+        };
+    }
 
     /**
      * Return click event handler for the picker.
      * Calls ctx.callback if provided.
-     */  
+     */
     function pickerListener(ctx, pickerElement) {
         return function(evt) {
             evt = evt || window.event;
             var mouse = mousePosition(evt),
-                width = pickerElement.offsetWidth,            
+                width = pickerElement.offsetWidth,
                 height = pickerElement.offsetHeight;
 
             ctx.s = mouse.x / width;
             ctx.v = (height - mouse.y) / height;
             var c = hsv2rgb(ctx);
             ctx.callback && ctx.callback(c.hex, { h: ctx.h - hueOffset, s: ctx.s, v: ctx.v }, { r: c.r, g: c.g, b: c.b }, mouse);
-        }
-    };
+        };
+    }
 
     var uniqID = 0;
-    
+
     /**
      * ColorPicker.
      * @param {DOMElement} slideElement HSV slide element.
@@ -222,7 +218,7 @@
      * @param {Function} callback Called whenever the color is changed provided chosen color in RGB HEX format as the only argument.
      */
     function ColorPicker(slideElement, pickerElement, callback) {
-        
+
         if (!(this instanceof ColorPicker)) return new ColorPicker(slideElement, pickerElement, callback);
 
         this.h = 0;
@@ -234,23 +230,20 @@
 
             var element = slideElement;
             element.innerHTML = colorpickerHTMLSnippet;
-            
+
             this.slideElement = element.getElementsByClassName('slide')[0];
             this.pickerElement = element.getElementsByClassName('picker')[0];
             var slideIndicator = element.getElementsByClassName('slide-indicator')[0];
             var pickerIndicator = element.getElementsByClassName('picker-indicator')[0];
-            
+
             ColorPicker.fixIndicators(slideIndicator, pickerIndicator);
 
             this.callback = function(hex, hsv, rgb, pickerCoordinate, slideCoordinate) {
-
                 ColorPicker.positionIndicators(slideIndicator, pickerIndicator, slideCoordinate, pickerCoordinate);
-                
                 pickerElement(hex, hsv, rgb);
             };
-            
+
         } else {
-        
             this.callback = callback;
             this.pickerElement = pickerElement;
             this.slideElement = slideElement;
@@ -263,20 +256,20 @@
 
             var slideClone = slide.cloneNode(true);
             var pickerClone = picker.cloneNode(true);
-            
+
             var hsvGradient = slideClone.getElementById('gradient-hsv');
-            
+
             var hsvRect = slideClone.getElementsByTagName('rect')[0];
-            
+
             hsvGradient.id = 'gradient-hsv-' + uniqID;
             hsvRect.setAttribute('fill', 'url(#' + hsvGradient.id + ')');
 
             var blackAndWhiteGradients = [pickerClone.getElementById('gradient-black'), pickerClone.getElementById('gradient-white')];
             var whiteAndBlackRects = pickerClone.getElementsByTagName('rect');
-            
+
             blackAndWhiteGradients[0].id = 'gradient-black-' + uniqID;
             blackAndWhiteGradients[1].id = 'gradient-white-' + uniqID;
-            
+
             whiteAndBlackRects[0].setAttribute('fill', 'url(#' + blackAndWhiteGradients[1].id + ')');
             whiteAndBlackRects[1].setAttribute('fill', 'url(#' + blackAndWhiteGradients[0].id + ')');
 
@@ -284,11 +277,10 @@
             this.pickerElement.appendChild(pickerClone);
 
             uniqID++;
-            
+
         } else {
-            
             this.slideElement.innerHTML = slide;
-            this.pickerElement.innerHTML = picker;            
+            this.pickerElement.innerHTML = picker;
         }
 
         addEventListener(this.slideElement, 'click', slideListener(this, this.slideElement, this.pickerElement));
@@ -296,16 +288,12 @@
 
         enableDragging(this, this.slideElement, slideListener(this, this.slideElement, this.pickerElement));
         enableDragging(this, this.pickerElement, pickerListener(this, this.pickerElement));
-    };
+    }
 
     function addEventListener(element, event, listener) {
-
         if (element.attachEvent) {
-            
             element.attachEvent('on' + event, listener);
-            
         } else if (element.addEventListener) {
-
             element.addEventListener(event, listener, false);
         }
     }
@@ -317,42 +305,38 @@
     * @param {Function} listener Function that will be called whenever mouse is dragged over the element with event object as argument.
     */
     function enableDragging(ctx, element, listener) {
-        
         var mousedown = false;
 
-        addEventListener(element, 'mousedown', function(evt) { mousedown = true;  });
-        addEventListener(element, 'mouseup',   function(evt) { mousedown = false;  });
-        addEventListener(element, 'mouseout',  function(evt) { mousedown = false;  });
+        addEventListener(element, 'mousedown', function() { mousedown = true; });
+        addEventListener(element, 'mouseup',   function() { mousedown = false; });
+        addEventListener(element, 'mouseout',  function() { mousedown = false; });
         addEventListener(element, 'mousemove', function(evt) {
-
             if (mousedown) {
-                
                 listener(evt);
             }
         });
     }
-
 
     ColorPicker.hsv2rgb = function(hsv) {
         var rgbHex = hsv2rgb(hsv);
         delete rgbHex.hex;
         return rgbHex;
     };
-    
+
     ColorPicker.hsv2hex = function(hsv) {
         return hsv2rgb(hsv).hex;
     };
-    
+
     ColorPicker.rgb2hsv = rgb2hsv;
 
     ColorPicker.rgb2hex = function(rgb) {
         return hsv2rgb(rgb2hsv(rgb)).hex;
     };
-    
+
     ColorPicker.hex2hsv = function(hex) {
         return rgb2hsv(ColorPicker.hex2rgb(hex));
     };
-    
+
     ColorPicker.hex2rgb = function(hex) {
         return { r: parseInt(hex.substr(1, 2), 16), g: parseInt(hex.substr(3, 2), 16), b: parseInt(hex.substr(5, 2), 16) };
     };
@@ -364,30 +348,28 @@
      * @param {object} rgb Object of the form: { r: <red>, g: <green>, b: <blue> }.
      * @param {string} hex String of the form: #RRGGBB.
      */
-     function setColor(ctx, hsv, rgb, hex) {
-         ctx.h = hsv.h % 360;
-         ctx.s = hsv.s;
-         ctx.v = hsv.v;
-         
-         var c = hsv2rgb(ctx);
-         
-         var mouseSlide = {
-             y: (ctx.h * ctx.slideElement.offsetHeight) / 360,
-             x: 0    // not important
-         };
-         
-         var pickerHeight = ctx.pickerElement.offsetHeight;
-         
-         var mousePicker = {
-             x: ctx.s * ctx.pickerElement.offsetWidth,
-             y: pickerHeight - ctx.v * pickerHeight
-         };
-         
-         ctx.pickerElement.style.backgroundColor = hsv2rgb({ h: ctx.h, s: 1, v: 1 }).hex;
-         ctx.callback && ctx.callback(hex || c.hex, { h: ctx.h, s: ctx.s, v: ctx.v }, rgb || { r: c.r, g: c.g, b: c.b }, mousePicker, mouseSlide);
-         
-         return ctx;
-    };
+    function setColor(ctx, hsv, rgb, hex) {
+        ctx.h = hsv.h % 360;
+        ctx.s = hsv.s;
+        ctx.v = hsv.v;
+
+        var c = hsv2rgb(ctx);
+
+        var mouseSlide = {
+            y: (ctx.h * ctx.slideElement.offsetHeight) / 360,
+            x: 0    // not important
+        };
+
+        var pickerHeight = ctx.pickerElement.offsetHeight;
+        var mousePicker = {
+            x: ctx.s * ctx.pickerElement.offsetWidth,
+            y: pickerHeight - ctx.v * pickerHeight
+        };
+        ctx.pickerElement.style.backgroundColor = hsv2rgb({ h: ctx.h, s: 1, v: 1 }).hex;
+        ctx.callback && ctx.callback(hex || c.hex, { h: ctx.h, s: ctx.s, v: ctx.v }, rgb || { r: c.r, g: c.g, b: c.b }, mousePicker, mouseSlide);
+
+        return ctx;
+    }
 
     /**
      * Sets color of the picker in rgb format.
@@ -396,7 +378,7 @@
     ColorPicker.prototype.setHsv = function(hsv) {
         return setColor(this, hsv);
     };
-    
+
     /**
      * Sets color of the picker in rgb format.
      * @param {object} rgb Object of the form: { r: <red>, g: <green>, b: <blue> }.
@@ -421,24 +403,22 @@
      * @param {object} mousePicker Coordinates of the mouse cursor in the picker area.
      */
     ColorPicker.positionIndicators = function(slideIndicator, pickerIndicator, mouseSlide, mousePicker) {
-        
         if (mouseSlide) {
             pickerIndicator.style.left = 'auto';
-            pickerIndicator.style.right = '0px';
-            pickerIndicator.style.top = '0px';
+            pickerIndicator.style.right = '0';
+            pickerIndicator.style.top = '0';
             slideIndicator.style.top = (mouseSlide.y - slideIndicator.offsetHeight/2) + 'px';
         }
         if (mousePicker) {
             pickerIndicator.style.top = (mousePicker.y - pickerIndicator.offsetHeight/2) + 'px';
             pickerIndicator.style.left = (mousePicker.x - pickerIndicator.offsetWidth/2) + 'px';
-        } 
+        }
     };
 
     /**
      * Helper to fix indicators - this is recommended (and needed) for dragable color selection (see enabledDragging()).
      */
     ColorPicker.fixIndicators = function(slideIndicator, pickerIndicator) {
-
         pickerIndicator.style.pointerEvents = 'none';
         slideIndicator.style.pointerEvents = 'none';
     };
