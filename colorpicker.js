f432c7b3ee3a07f2c101de747d4ff339346ff958
zvin <zvin@free.fr>
2014-04-20 19:26:51 +0200

    dragging works even if you get the mouse out of the element

--- colorpicker.js
+++ colorpicker.js
@@ -20,17 +20,19 @@
         '</div>'
     ].join('');
 
+    function restrictToBounds(value, minimum, maximum) {
+        return Math.max(Math.min(value, maximum), minimum)
+    }
+
     /**
      * Return mouse position relative to the element el.
      */
-    function mousePosition(evt) {
-        // Webkit:
-        if (evt.offsetX !== undefined && evt.offsetY !== undefined) {
-            return { x: evt.offsetX, y: evt.offsetY };
-        }
-        // Firefox:
-        var wrapper = evt.target.parentNode.parentNode;
-        return { x: evt.layerX - wrapper.offsetLeft, y: evt.layerY - wrapper.offsetTop };
+    function mousePosition(evt, element) {
+        var rect = element.getBoundingClientRect();
+        return {
+            x: restrictToBounds(evt.clientX - rect.left, 0, element.clientWidth),
+            y: restrictToBounds(evt.clientY - rect.top, 0, element.clientHeight)
+        };
     }
 
     /**
@@ -153,7 +155,7 @@
      */
     function slideListener(ctx, slideElement, pickerElement) {
         return function(evt) {
-            var mouse = mousePosition(evt);
+            var mouse = mousePosition(evt, slideElement);
             ctx.h = mouse.y / slideElement.offsetHeight * 360 + hueOffset;
             var pickerColor = hsv2rgb({ h: ctx.h, s: 1, v: 1 });
             var c = hsv2rgb({ h: ctx.h, s: ctx.s, v: ctx.v });
@@ -168,7 +170,7 @@
      */
     function pickerListener(ctx, pickerElement) {
         return function(evt) {
-            var mouse = mousePosition(evt),
+            var mouse = mousePosition(evt, pickerElement),
                 width = pickerElement.offsetWidth,
                 height = pickerElement.offsetHeight;
 
@@ -246,30 +248,33 @@
 
         uniqID++;
 
-        this.slideElement.addEventListener('click', slideListener(this, this.slideElement, this.pickerElement), false);
-        this.pickerElement.addEventListener('click', pickerListener(this, this.pickerElement), false);
-
-        enableDragging(this, this.slideElement, slideListener(this, this.slideElement, this.pickerElement));
-        enableDragging(this, this.pickerElement, pickerListener(this, this.pickerElement));
+        enableDragging(this.slideElement, slideListener(this, this.slideElement, this.pickerElement));
+        enableDragging(this.pickerElement, pickerListener(this, this.pickerElement));
     }
 
    /**
     * Enable drag&drop color selection.
-    * @param {object} ctx ColorPicker instance.
     * @param {DOMElement} element HSV slide element or HSV picker element.
     * @param {Function} listener Function that will be called whenever mouse is dragged over the element with event object as argument.
     */
-    function enableDragging(ctx, element, listener) {
+    function enableDragging(element, listener) {
         var mousedown = false;
-
-        element.addEventListener('mousedown', function(evt) { mousedown = true; evt.preventDefault() }, false);
-        element.addEventListener('mouseup',   function() { mousedown = false; }, false);
-        element.addEventListener('mouseout',  function() { mousedown = false; }, false);
-        element.addEventListener('mousemove', function(evt) {
-            if (mousedown) {
-                listener(evt);
-            }
-        }, false);
+        function mouseDown(evt) {
+            mousedown = true;
+            evt.preventDefault();
+            // listen to mouse move/up on document only after mouse down on element
+            document.addEventListener('mouseup', mouseUp, false);
+            document.addEventListener('mousemove', mouseMove, false);
+        }
+        function mouseMove(evt) {if (mousedown) listener(evt);}
+        function mouseUp(evt) {
+            mouseMove(evt);
+            mousedown = false;
+            // stop listening to mouse move/up on document after mouse up
+            document.removeEventListener('mouseup', mouseUp, false);
+            document.removeEventListener('mousemove', mouseMove, false);
+        }
+        element.addEventListener('mousedown', mouseDown, false);
     }
 
     ColorPicker.hsv2rgb = function(hsv) {
